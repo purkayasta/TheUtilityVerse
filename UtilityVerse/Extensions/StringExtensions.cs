3891ebe57eebb7b257b3af1c8314344a27215406
purkayasta <pritompurkayasta@gmail.com>
2023-03-25 22:27:06 +0600

    added string extensions

--- UtilityVerse/Extensions/StringExtensions.cs
+++ UtilityVerse/Extensions/StringExtensions.cs
@@ -1,8 +1,34 @@
-﻿namespace UtilityVerse.Extensions
+﻿using System.Text;
+
+namespace UtilityVerse.Extensions
 {
     public static class StringExtensions
     {
-        public static bool IsNullOrWhiteSpace(this string val)
-            => string.IsNullOrEmpty(val) || string.IsNullOrWhiteSpace(val.Trim());
+        public static bool IsNullEmptyOrWhiteSpace(this string? val)
+        {
+            if (val == null) return false;
+            return string.IsNullOrEmpty(val) || string.IsNullOrWhiteSpace(val.Trim());
+        }
+
+
+        public static string AsString(this string[]? strs, string? separator = null)
+        {
+            if (strs is null || strs.Length < 1) return string.Empty;
+
+            var sb = new StringBuilder();
+
+            bool isSeparatorExist = !separator.IsNullEmptyOrWhiteSpace();
+
+            for (int index = 0; index < strs.Length; index++)
+            {
+                sb.Append(strs[index]);
+
+                if (isSeparatorExist)
+                    if (index < strs.Length - 1)
+                        sb.Append(separator);
+            }
+
+            return sb.ToString();
+        }
     }
 }
