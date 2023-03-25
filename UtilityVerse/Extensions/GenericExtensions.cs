a7d3805fc8760bf1db396ffcaa338001225836ae
purkayasta <pritompurkayasta@gmail.com>
2023-03-25 22:26:35 +0600

    added generic extensions

---
+++ UtilityVerse/Extensions/GenericExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+
+namespace UtilityVerse.Extensions
+{
+    public static class GenericExtensions
+    {
+        public static T? To<T>(this byte[]? byteArr)
+        {
+            ArgumentNullException.ThrowIfNull(byteArr);
+            return JsonSerializer.Deserialize<T>(byteArr);
+        }
+
+        public static byte[] ToByteArray<T>(this T? instance)
+        {
+            ArgumentNullException.ThrowIfNull(instance);
+            return Encoding.UTF8.GetBytes(JsonSerializer.Serialize<T>(instance));
+        }
+    }
+}
