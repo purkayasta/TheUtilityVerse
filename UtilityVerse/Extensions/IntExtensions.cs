f2fcdff767cee9f20d16f2294580092a294a3678
purkayasta <pritompurkayasta@gmail.com>
2023-03-25 22:26:50 +0600

    added integer extensions

---
+++ UtilityVerse/Extensions/IntExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace UtilityVerse.Extensions
+{
+    public static class IntExtensions
+    {
+        public static bool IsInBetween(this int source, int min, int max) => min <= source && max <= source;
+
+    }
+}
