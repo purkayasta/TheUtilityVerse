8dac0de97de96ca26ed2f5a6ec694a8b6fed8d81
purkayasta <pritompurkayasta@gmail.com>
2023-04-14 19:59:54 +0600

    added integer extension tests

--- src/UtilityVerse/Extensions/IntExtension.cs
+++ src/UtilityVerse/Extensions/IntExtension.cs
@@ -15,7 +15,22 @@
             ArgumentNullException.ThrowIfNull(min, nameof(min));
             ArgumentNullException.ThrowIfNull(max, nameof(max));
 
-            return source.Value >= min && source.Value <= max;
+            return IsInBetween(source.Value, min.Value, max.Value);
+        }
+
+        /// <summary>
+        /// This method will help to determine that source number is in between or not.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static bool IsInBetween(this int source, int min, int max)
+        {
+            if (min > max)
+                throw new ArgumentOutOfRangeException(nameof(min) + " cannot be greater than " + nameof(max));
+
+            return source >= min && source <= max;
         }
     }
 }
---
+++ test/UtilityVerse.Test/IntExtensionTest.cs
@@ -0,0 +1,49 @@
+﻿using UtilityVerse.Extensions;
+
+namespace UtilityVerse.UnitTesting
+{
+    public class IntExtensionTest
+    {
+        [Fact]
+        public void IsInBetween_ThrowException_IfSourceIsNull()
+        {
+            int? source = default;
+            Assert.Throws<ArgumentNullException>(() => source.IsInBetween(default, default));
+        }
+
+        [Fact]
+        public void IsInBetween_ThrowException_IfMinIsNull()
+        {
+            int? source = 0;
+            Assert.Throws<ArgumentNullException>(() => source.IsInBetween(default, default));
+        }
+
+        [Fact]
+        public void IsInBetween_ThrowException_IfMaxIsNull()
+        {
+            int? source = 0;
+            Assert.Throws<ArgumentNullException>(() => source.IsInBetween(1, default));
+        }
+
+        [Fact]
+        public void IsInBetween_ThrowException_WhenMinIsGreaterThanMax()
+        {
+            int? source = 0;
+            Assert.Throws<ArgumentOutOfRangeException>(() => source.IsInBetween(1, 0));
+        }
+
+        [Fact]
+        public void IsInBetween_ReturnFalse_WhenWrongDataIsProvided()
+        {
+            int? source = 10;
+            Assert.False(source.IsInBetween(1, 9));
+        }
+
+        [Fact]
+        public void IsInBetween_ReturnTrue_WhenRightDataIsProvided()
+        {
+            int? source = 100;
+            Assert.True(source.IsInBetween(10, 200));
+        }
+    }
+}
