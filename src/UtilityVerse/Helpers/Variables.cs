f5a0f41c5a86c64827e871b6d4d930a14da83453
purkayasta <pritompurkayasta@gmaill.com>
2023-06-13 22:49:07 +0600

    added string support

---
+++ src/UtilityVerse/Helpers/Variables.cs
@@ -0,0 +1,8 @@
+﻿namespace UtilityVerse.Helpers;
+
+internal static class Variables
+{
+    internal const string IpAddressRegex = @"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b";
+    internal const string EmailAddressRegex = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+    internal const string InvalidCharacterRegex = @"[^a-z0-9\s-]";
+}
