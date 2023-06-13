a0cf0780469d41ae53733e5194d0357efbed1924
purkayasta <pritompurkayasta@gmaill.com>
2023-06-13 22:48:54 +0600

    added pagination list support

---
+++ src/UtilityVerse/Contracts/PagedList.cs
@@ -0,0 +1,9 @@
+﻿namespace UtilityVerse.Contracts;
+
+public class PagedList<T>
+{
+    public bool IsValid { get; set; }
+    public int Count { get; set; }
+    public IEnumerable<T> Results { get; set; } = Enumerable.Empty<T>();
+    public string? Errors { get; set; } = string.Empty;
+}
---
+++ src/UtilityVerse/Extensions/PagedListExtension.cs
@@ -0,0 +1,34 @@
+﻿using UtilityVerse.Contracts;
+
+namespace UtilityVerse.Extensions;
+
+public static class PagedListExtension
+{
+    public static PagedList<T> ToPagedList<T>(this IQueryable<T> queryable, int index, int pageSize)
+    {
+        if (index == default || pageSize == default)
+            throw new InvalidDataException($"{nameof(index)} or {nameof(pageSize)} params value is invalid");
+
+        var pagedList = new PagedList<T>();
+        try
+        {
+            var result = queryable.Take(pageSize).Skip(index * pageSize).ToList();
+            pagedList.Results = result;
+            pagedList.Count = result.Count;
+            pagedList.IsValid = true;
+        }
+        catch (Exception exception)
+        {
+            pagedList.IsValid = false;
+            pagedList.Errors = ExtractExceptionError(exception);
+        }
+
+        return pagedList;
+    }
+
+
+    private static string ExtractExceptionError(Exception? exception)
+        => exception is null
+            ? string.Empty
+            : $"Exception Message: {exception.Message}. Inner Exception: {exception.InnerException?.Message}";
+}
