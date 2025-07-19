767871a7a6a2b276dd6b4ee8722963940525f113
purkayasta <pritompurkayasta@gmail.com>
2025-07-19 13:21:27 +0600

    COPY: Updated The validation error analyzer

---
+++ src/UtilityVerse.Copy/Analyzers/ValidationAnalyzer.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace UtilityVerse.Copy.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class ValidationAnalyzer : DiagnosticAnalyzer
+{
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+        [Rules.MissingPartialRule, Rules.DeepRule, Rules.ShallowRule];
+    
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+
+        context.RegisterCompilationStartAction(compilationContext =>
+        {
+            compilationContext.RegisterSymbolAction(AnalyzeNamedType, SymbolKind.NamedType);
+        });
+    }
+
+    private static void AnalyzeNamedType(SymbolAnalysisContext context)
+    {
+        if (context.Symbol is not INamedTypeSymbol namedType)
+            return;
+
+        if (namedType.TypeKind is not (TypeKind.Class or TypeKind.Struct))
+            return;
+
+        if (namedType.GetAttributes().Any(attr =>
+            attr.AttributeClass?.ToDisplayString() is
+                "System.CodeDom.Compiler.GeneratedCodeAttribute" or
+                "System.Runtime.CompilerServices.CompilerGeneratedAttribute"))
+            return;
+
+        var syntaxRef = namedType.DeclaringSyntaxReferences.FirstOrDefault();
+        if (syntaxRef?.GetSyntax() is not TypeDeclarationSyntax typeSyntax)
+            return;
+
+        var hasDeepCopy = Helper.HasDeepCopyOptIn(namedType);
+        var hasShallowCopy = Helper.HasShallowCopyOptIn(namedType);
+        
+        if ((hasDeepCopy || hasShallowCopy) && !Helper.IsPartial(namedType))
+        {
+            var diagnostic = Diagnostic.Create(Rules.MissingPartialRule, typeSyntax.Identifier.GetLocation(), namedType.Name);
+            context.ReportDiagnostic(diagnostic);
+        }
+
+        if (!hasDeepCopy && !hasShallowCopy)
+            return;
+
+        foreach (var prop in Helper.GetAllProperties(namedType).Where(p => !p.IsStatic))
+        {
+            AnalyzeTypeRecursive(
+                context,
+                prop.Type,
+                prop.Locations.FirstOrDefault() ?? typeSyntax.Identifier.GetLocation(),
+                prop.Name,
+                namedType,
+                hasDeepCopy,
+                hasShallowCopy
+            );
+        }
+    }
+
+    private static void AnalyzeTypeRecursive(
+        SymbolAnalysisContext context,
+        ITypeSymbol type,
+        Location diagnosticLocation,
+        string propertyName,
+        INamedTypeSymbol parentType,
+        bool requireDeepCopy,
+        bool requireShallowCopy)
+    {
+        if (type is not INamedTypeSymbol namedType)
+            return;
+
+        var unwrapped = UnwrapNullable(namedType);
+
+        if (Helper.IsTrulyPrimitive(unwrapped))
+            return;
+
+        if (Helper.IsGenericCollection(unwrapped.OriginalDefinition.ToDisplayString(), out _))
+        {
+            foreach (var typeArg in unwrapped.TypeArguments.OfType<ITypeSymbol>())
+            {
+                AnalyzeTypeRecursive(context, typeArg, diagnosticLocation, propertyName, parentType, requireDeepCopy, requireShallowCopy);
+            }
+            return;
+        }
+
+        if (requireDeepCopy && !Helper.HasDeepCopyOptIn(unwrapped))
+        {
+            var diag = Diagnostic.Create(Rules.DeepRule, diagnosticLocation,
+                propertyName, parentType.Name, unwrapped.Name);
+            context.ReportDiagnostic(diag);
+        }
+
+        if (requireShallowCopy && !Helper.HasShallowCopyOptIn(unwrapped))
+        {
+            var diag = Diagnostic.Create(Rules.ShallowRule, diagnosticLocation,
+                propertyName, parentType.Name, unwrapped.Name);
+            context.ReportDiagnostic(diag);
+        }
+    }
+
+    private static INamedTypeSymbol UnwrapNullable(INamedTypeSymbol type)
+    {
+        return type.OriginalDefinition.SpecialType == SpecialType.System_Nullable_T &&
+               type.TypeArguments.FirstOrDefault() is INamedTypeSymbol underlying
+            ? underlying
+            : type;
+    }
+
+    private static bool IsInsideCopyEnabledPartialParent(INamedTypeSymbol? symbol)
+    {
+        while (symbol is not null)
+        {
+            if (!Helper.IsPartial(symbol))
+                return false;
+
+            if (Helper.HasDeepCopyOptIn(symbol) || Helper.HasShallowCopyOptIn(symbol))
+                return true;
+
+            symbol = symbol.ContainingType;
+        }
+        return false;
+    }
+}
--- src/UtilityVerse.Copy/Helper.cs
+++ src/UtilityVerse.Copy/Helper.cs
@@ -1,9 +1,3 @@
-/// <summary>
-/// Author: Pritom Purkayasta
-//  Copyright (c) Pritom Purkayasta All rights reserved.
-//  FREE TO USE TO CONNECT THE WORLD
-/// </summary>
-
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis;
@@ -22,19 +16,19 @@ internal static class Helper
         return symbol.SpecialType switch
         {
             SpecialType.System_Boolean or
-                SpecialType.System_Byte or
-                SpecialType.System_SByte or
-                SpecialType.System_Int16 or
-                SpecialType.System_UInt16 or
-                SpecialType.System_Int32 or
-                SpecialType.System_UInt32 or
-                SpecialType.System_Int64 or
-                SpecialType.System_UInt64 or
-                SpecialType.System_Single or
-                SpecialType.System_Double or
-                SpecialType.System_Char or
-                SpecialType.System_Enum or
-                SpecialType.System_String => true,
+            SpecialType.System_Byte or
+            SpecialType.System_SByte or
+            SpecialType.System_Int16 or
+            SpecialType.System_UInt16 or
+            SpecialType.System_Int32 or
+            SpecialType.System_UInt32 or
+            SpecialType.System_Int64 or
+            SpecialType.System_UInt64 or
+            SpecialType.System_Single or
+            SpecialType.System_Double or
+            SpecialType.System_Char or
+            SpecialType.System_Enum or
+            SpecialType.System_String => true,
             _ => false
         };
     }
@@ -43,7 +37,6 @@ internal static class Helper
     {
         kind = originalDef switch
         {
-            // List-like collections
             "System.Collections.Generic.List<T>" => "List",
             "System.Collections.Generic.IList<T>" => "List",
             "System.Collections.ObjectModel.Collection<T>" => "List",
@@ -51,30 +44,25 @@ internal static class Helper
             "System.Collections.ObjectModel.ObservableCollection<T>" => "List",
             "System.ComponentModel.BindingList<T>" => "List",
             "System.Collections.Immutable.ImmutableList<T>" => "List",
-            "System.Collections.Generic.ReadOnlyList<T>" => "List", // custom/unofficial
+            "System.Collections.Generic.ReadOnlyList<T>" => "List",
 
-            // Enumerable-like collections
             "System.Collections.Generic.IEnumerable<T>" => "Enumerable",
             "System.Collections.Generic.ICollection<T>" => "Enumerable",
             "System.Collections.Generic.IReadOnlyCollection<T>" => "Enumerable",
             "System.Collections.Generic.IReadOnlyList<T>" => "Enumerable",
 
-            // Hash-based collections
             "System.Collections.Generic.HashSet<T>" => "HashSet",
 
-            // Dictionary-like collections
             "System.Collections.Generic.Dictionary<TKey, TValue>" => "Dictionary",
             "System.Collections.Frozen.FrozenDictionary<TKey, TValue>" => "Dictionary",
             "System.Collections.Concurrent.ConcurrentDictionary<TKey, TValue>" => "Dictionary",
 
-            // Immutable
             "System.Collections.Immutable.ImmutableArray<T>" => "List",
             "System.Collections.Immutable.ImmutableSortedSet<T>" => "HashSet",
             "System.Collections.Immutable.ImmutableHashSet<T>" => "HashSet",
             "System.Collections.Immutable.ImmutableDictionary<TKey, TValue>" => "Dictionary",
             "System.Collections.Immutable.ImmutableSortedDictionary<TKey, TValue>" => "Dictionary",
 
-            // Queue / Stack
             "System.Collections.Generic.Queue<T>" => "Queue",
             "System.Collections.Generic.Stack<T>" => "Stack",
 
@@ -107,13 +95,22 @@ internal static class Helper
 
     internal static bool HasDeepCopyOptIn(INamedTypeSymbol typeSymbol)
     {
-        return typeSymbol.GetAttributes().Any(a => a.AttributeClass?.ToDisplayString() == "UtilityVerse.Copy.DeepCopy") ||
-               typeSymbol.AllInterfaces.Any(i => i.ToDisplayString() == "UtilityVerse.Copy.IDeepCopy");
+        return typeSymbol.GetAttributes().Any(a =>
+                   SymbolEquals(a.AttributeClass, "UtilityVerse.Copy.DeepCopy")) ||
+               typeSymbol.AllInterfaces.Any(i =>
+                   SymbolEquals(i, "UtilityVerse.Copy.IDeepCopy"));
     }
 
     internal static bool HasShallowCopyOptIn(INamedTypeSymbol typeSymbol)
     {
-        return typeSymbol.GetAttributes().Any(a => a.AttributeClass?.ToDisplayString() == "UtilityVerse.Copy.ShallowCopy") ||
-               typeSymbol.AllInterfaces.Any(i => i.ToDisplayString() == "UtilityVerse.Copy.IShallowCopy");
+        return typeSymbol.GetAttributes().Any(a =>
+                   SymbolEquals(a.AttributeClass, "UtilityVerse.Copy.ShallowCopy")) ||
+               typeSymbol.AllInterfaces.Any(i =>
+                   SymbolEquals(i, "UtilityVerse.Copy.IShallowCopy"));
+    }
+
+    private static bool SymbolEquals(ISymbol? symbol, string fullyQualifiedMetadataName)
+    {
+        return symbol?.ToDisplayString() == fullyQualifiedMetadataName;
     }
 }
